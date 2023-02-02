9245d783a56f2c44e1b4d6fdb8b6eec3d45a7582
Ryan Champin <rwchampin@gmail.com>
2023-02-02 15:34:43 -0500

    fghffh

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ import { watch } from 'vue'
 import gsap from 'gsap'
 import * as THREE from 'three'
 import { useMouse } from '@vueuse/core'
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import MagicMouse from './core/MagicMouse'
 import { useMagicKeyRegistration } from '~/composables/useMagicKeyRegistration'
 // import { useVolumetric } from '~/composables/useVolumetric.js'
@@ -37,7 +37,10 @@ useHead({
   ],
 
 })
-let mousePointLight; let scene; let camera; let renderer; let ambientLight; let pointLight; let directionalLight; let rectAreaLight; let spotLight; let hemisphereLight
+let scene; let camera; let renderer
+let ambientLight; let pointLight
+// let directionalLight;let rectAreaLight;
+// let spotLight; let hemisphereLight
 
 const { debugMode } = nuxtApp.$appStore
 
@@ -48,14 +51,130 @@ const animate = () => {
 onMounted(() => {
   // nextTick(() => {
   const { createCore } = useCore()
-  const core = createCore()
+  const core = createCore({
+
+  })
   scene = core.scene
   camera = core.camera
   camera.lookAt(0, 0, 0)
   renderer = core.renderer
   const magicMouse = new MagicMouse({ scene, camera, renderer })
   // ParticleController(renderer)
-  // useVolumetricetric({ scene, camera, renderer })
+  // useVolumetric({ scene, camera, renderer })
+  if (!window.Float32Array)
+    window.Float32Array = Array
+
+  const WIDTH = window.innerWidth // px
+  const HEIGHT = window.innerHeight // px
+  const NPARTICLES = 10000
+  const CELLSIZE = 20
+  const CELLSIZE2 = CELLSIZE / 2
+  const canvas = renderer.domElement
+  const screenRatio = 1.0
+
+  const ctx = renderer.getContext()
+  const particles = new Float32Array(NPARTICLES * 4)
+  const flow = new Float32Array(WIDTH * HEIGHT / CELLSIZE / CELLSIZE * 2)
+  const CELLS_X = WIDTH / 20
+  const floor = Math.floor
+
+  function Particle(x, y) {
+    this.x = x
+    this.y = y
+    this.vx = 0
+    this.vy = 0
+  }
+
+  for (let i = 0; i < particles.length;) {
+    particles[i++] = Math.random() * WIDTH
+    particles[i++] = Math.random() * HEIGHT
+    particles[i++] = 0
+    particles[i++] = 0
+  }
+  for (let i = 0; i < flow.length; i++)
+    flow[i] = 0
+
+  const start = { x: 0, y: 0 }; let down = false
+  canvas.onmousedown = function (e) {
+    start.x = (e.clientX - canvas.offsetLeft) * screenRatio
+    start.y = e.clientY - canvas.offsetTop * screenRatio
+    down = true
+  }
+  canvas.ontouchstart = function (e) {
+    canvas.onmousedown(e.touches[0])
+    return false
+  }
+  canvas.onmouseup = canvas.ontouchend = function () {
+    down = false
+  }
+  canvas.ontouchmove = function (e) {
+    canvas.onmousemove(e.touches[0])
+  }
+
+  canvas.onmousemove = function (e) {
+    const mx = (e.clientX - canvas.offsetLeft) * screenRatio
+    const my = (e.clientY - canvas.offsetTop) * screenRatio
+    if (!down || mx == start.x && my == start.y)
+      return
+    const ai = (floor(mx / CELLSIZE)
+      + floor(my / CELLSIZE) * floor(WIDTH / CELLSIZE)) * 2
+    flow[ai] += (mx - start.x) * 0.4
+    flow[ai + 1] += (my - start.y) * 0.4
+    start.x = mx
+    start.y = my
+  }
+
+  setInterval(() => {
+    let x; let y; let vx; let vy; const vd = 0.95; const ax; const ay; const ai; const ad = 0.95; const ar = 0.004
+    const w1 = WIDTH - 1
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.6)'
+    ctx.globalCompositeOperation = 'source-over'
+    ctx.fillRect(0, 0, WIDTH, HEIGHT)
+    ctx.fillStyle = 'rgba(100, 100, 255, 0.8)'
+    ctx.globalCompositeOperation = 'lighter'
+    for (let i = 0, l = particles.length; i < l; i += 4) {
+      x = particles[i]
+      y = particles[i + 1]
+      vx = particles[i + 2]
+      vy = particles[i + 3]
+      ai = (~~(x / CELLSIZE) + ~~(y / CELLSIZE) * CELLS_X) * 2
+      ax = flow[ai]
+      ay = flow[ai + 1]
+
+      ax = (ax + vx * ar) * ad
+      ay = (ay + vy * ar) * ad
+      vx = (vx + ax) * vd
+      vy = (vy + ay) * vd
+      x += vx
+      y += vy
+      ctx.fillRect(~~x, ~~y, 2, 2)
+
+      if (x < 0) {
+        vx *= -1
+        x = 0
+      }
+      else if (x > w1) {
+        x = w1
+        vx *= -1
+      }
+
+      if (y < 0) {
+        vy *= -1
+        y = 0
+      }
+      else if (y > HEIGHT) {
+        y = HEIGHT - 1
+        vy *= -1
+      }
+
+      particles[i] = x
+      particles[i + 1] = y
+      particles[i + 2] = vx
+      particles[i + 3] = vy
+      flow[ai] = ax
+      flow[ai + 1] = ay
+    }
+  }, 33)
 
   ambientLight = core.ambientLight
   pointLight = core.pointLight
@@ -83,11 +202,13 @@ onMounted(() => {
       scene.add(axesHelper)
     }
   })
+  const controls = new OrbitControls(camera, renderer.domElement)
 })
 </script>
 
 <template>
   <ClientOnly>
+    <div id="cursor" />
     <TheFloatingMenu />
     <TheDarkScrollbar />
   </ClientOnly>
