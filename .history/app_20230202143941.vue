dba6088fd29485fe434e8c1828eb622649d91920
Ryan Champin <rwchampin@gmail.com>
2023-02-02 15:35:00 -0500

    hhgjh

---
+++ .history/app_20230202143941.vue
@@ -0,0 +1,310 @@
+<script setup>
+import { watch } from 'vue'
+import gsap from 'gsap'
+import * as THREE from 'three'
+import { useMouse } from '@vueuse/core'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import MagicMouse from './core/MagicMouse'
+import { useMagicKeyRegistration } from '~/composables/useMagicKeyRegistration'
+// import { useVolumetric } from '~/composables/useVolumetric.js'
+import { Utils } from '~/utils'
+// import MoverParticle fromx '~/particles/MoverParticle.js'
+import { useCore } from '~~/composables/useCore'
+// import { ParticleController } from '~/particles/ParticleSystem/ParticleController'
+const nuxtApp = useNuxtApp()
+const { x, y, sourceType } = useMouse()
+// nuxtApp.vueApp.use(Naive)
+const canvas2d = ref(null)
+const canvas3d = ref(null)
+
+useHead({
+  title: 'RYAN THE DEVELOPER',
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: 'Ryan the developer' },
+    { name: 'format-detection', content: 'telephone=no' },
+    { name: 'msapplication-TileColor', content: '#da532c' },
+    { name: 'theme-color', content: '#ffffff' },
+  ],
+  link: [
+    {
+      rel: 'icon', type: 'image/png', href: '/favicon.ico',
+    },
+  ],
+
+})
+let scene; let camera; let renderer
+let ambientLight; let pointLight
+// let directionalLight;let rectAreaLight;
+// let spotLight; let hemisphereLight
+
+const { debugMode } = nuxtApp.$appStore
+
+const animate = () => {
+  // animateIcosahedron()
+  renderer.render(scene, camera)
+}
+onMounted(() => {
+  // nextTick(() => {
+  const { createCore } = useCore()
+  const core = createCore({
+
+  })
+  scene = core.scene
+  camera = core.camera
+  camera.lookAt(0, 0, 0)
+  renderer = core.renderer
+  const magicMouse = new MagicMouse({ scene, camera, renderer })
+  // ParticleController(renderer)
+  useVolumetricetric({ scene, camera, renderer })
+
+  ambientLight = core.ambientLight
+  pointLight = core.pointLight
+
+  // useLogo({ scene, camera, renderer })
+
+  Utils.three.sceneAdd(scene, pointLight, ambientLight, camera)
+
+  gsap.ticker.add((time, deltaTime, frame) => {
+    // camera.position.y += (-y + 200 - camera.position.y) * 0.05
+    // camera.lookAt(scene.position)
+    animate()
+  })
+  gsap.to('.canvas-ui', {
+    opacity: 1,
+    duration: 1,
+    ease: 'power2.out',
+  })
+  useMagicKeyRegistration()
+  watch(debugMode, (debugMode) => {
+    if (debugMode) {
+      Utils.tweakpane.addScene(scene)
+      Utils.tweakpane.addCamera(camera)
+      const axesHelper = new THREE.AxesHelper(5)
+      scene.add(axesHelper)
+    }
+  })
+  const controls = new OrbitControls(camera, renderer.domElement)
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <div id="cursor" />
+    <TheFloatingMenu />
+    <TheDarkScrollbar />
+  </ClientOnly>
+  <Teleport to="body">
+    <canvas ref="canvas2d" class="canvas-ui main-canvas-2d" />
+    <canvas ref="canvas3d" class="canvas-ui main-canvas-3d" />
+  </Teleport>
+  <div id="smooth-wrapper">
+    <div id="smooth-content">
+      <ClientOnly>
+        <TheContent>
+          <slot />
+        </TheContent>
+      </ClientOnly>
+    </div>
+  </div>
+  <!--
+            <div id="controlContainer1" class="controlContainer">
+              <div class="showHideControls">
+                <p>Toggle</p>
+              </div>
+              <div id="welcomeMessage" class="controls">
+                <div id="shortcuts">
+                  <div>
+                    <ul>
+                      <li>A : toggle accelerations</li>
+                      <li>P : toggle particles</li>
+                      <li>V : toggle velocities</li>
+                      <li>O : toggle objects</li>
+                      <li>E : add emitter</li>
+                      <li>F : add field</li>
+                      <li>C : Clear</li>
+                      <li>S : Step frame</li>
+                      <li>Space : pause</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div id="controlContainer2" class="controlContainer">
+              <div class="showHideControls">
+                <p>Toggle Controls</p>
+              </div>
+              <div id="displayControls" class="controls">
+                <ul>
+                  <li>
+                    <button id="addEmitter">
+                      New Emitter
+                    </button>
+                  </li>
+                  <li>
+                    <button id="addField">
+                      New Field
+                    </button>
+                  </li>
+                  <li>Click Behavior</li>
+                  <li>
+                    <div id="clickBehavior">
+                      <input id="clickBehavior1" type="radio" name="clickBehavior" checked="checked"><label
+                        for="clickBehavior1"
+                      >Repel</label>
+                      <input id="clickBehavior2" type="radio" name="clickBehavior"><label for="clickBehavior2">Attract</label>
+                      <input id="clickBehavior3" type="radio" name="clickBehavior"><label for="clickBehavior3">None</label>
+                    </div>
+                  </li>
+                  <li>&nbsp;</li>
+                  <li>
+                    <button id="startStop">
+                      Stop
+                    </button>
+                  </li>
+                  <li>
+                    <button id="step">
+                      Step Frame
+                    </button>
+                  </li>
+                  <li>
+                    <button id="objects">
+                      Hide objects
+                    </button>
+                  </li>
+                  <li>
+                    <button id="info">
+                      Hide info
+                    </button>
+                  </li>
+                  <li>
+                    <button id="particles">
+                      Hide particles
+                    </button>
+                  </li>
+                  <li>
+                    <button id="accelerations">
+                      Show accelerations
+                    </button>
+                  </li>
+                  <li>
+                    <button id="velocities">
+                      Show velocities
+                    </button>
+                  </li>
+                  <li>
+                    <button id="clear">
+                      Clear
+                    </button>
+                  </li>
+                  <li>
+                    <button id="strings">
+                      Strings
+                    </button>
+                  </li>
+                  <li>Max Particles :</li>
+                  <li id="maxParticles">
+                    <button value="10">
+                      10
+                    </button>
+                    <button value="2000">
+                      2k
+                    </button>
+                    <button value="5000">
+                      5k
+                    </button>
+                    <button value="10000">
+                      10k
+                    </button>
+                    <button value="20000">
+                      20k
+                    </button>
+                  </li>
+                  <li>Particle Style</li>
+                  <li id="particleStyle">
+                    <button value="basic">
+                      Basic
+                    </button>
+                    <button value="variable">
+                      Colors
+                    </button>
+                    <button value="fancy">
+                      Fancy
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div id="footer">
+              <button id="save">
+                Save Locally
+              </button>
+              <button id="load">
+                Load Locally
+              </button>
+
+              -
+              <button id="example1" class="loadExample">
+                Example 1
+              </button>
+              <button id="example2" class="loadExample">
+                Example 2
+              </button>
+              <button id="example3" class="loadExample">
+                Example 3
+              </button>
+              <button id="example4" class="loadExample">
+                Example 4
+              </button>
+              <button id="example5" class="loadExample">
+                Example 5
+              </button>
+              <button id="bonus" class="loadExample">
+                Candle
+              </button>
+              <button id="3alt" class="loadExample">
+                Ex. 3 Alt
+              </button>
+            </div>
+            <div id="floatingControls" class="closable">
+              <button id="closeFloatingControls" />
+              <button id="deleteObject" />
+              <div id="variableControl">
+                raierstientrsh
+              </div>
+            </div> -->
+
+  <!-- <TheDebugger v-if="nuxtApp.$appStore.debugMode" /> -->
+</template>
+
+<style scoped>
+.main-canvas-3d,
+#canvas-container {
+  position: fixed !important;
+  top: 0;
+  left: 0;
+  width: 100vw !important;
+  height: 100vh !important;
+  z-index: 99 !important;
+  background-color: transparent !important;
+}
+
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
---
+++ .history/core/MagicMouse_20230202015858.js
@@ -0,0 +1,208 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js'
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js'
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js'
+import { Vector2 } from '~/utils/Vector'
+import fragment from '~/shaders/mouseGlow/fragment.glsl'
+import vertex from '~/shaders/mouseGlow/vertex.glsl'
+import { Utils } from '~/utils'
+
+export default class MagicMouse {
+  constructor({ camera, scene, renderer }) {
+    /*******************
+     * 1. Setup
+     * *****************/
+    this.camera = camera
+    this.scene = scene
+    this.renderer = renderer
+
+    /*******************
+     * 2. Properties
+     * *****************/
+    this.mouse = new Vector2()
+    this.elasticMouse = new Vector2()
+    this.elasticMouseVel = new Vector2()
+    this.eMouse = new Vector2()
+    this.temp = new Vector2()
+    this.cursor = document.createElement('div')
+    this.prevPos = new Vector2()
+    this.radius = Vector2.random2D()
+    this.vel = Vector2.random2D()
+    this.acc = new Vector2(0, 0)
+    this.maxSpeed = 5
+    this.maxForce = 0.1
+    this.color = 0xFFFFFF
+    this.sphereSize = 0.005
+    this.mouseLightIntensity = 1000
+    this.params = {
+      exposure: 1,
+      bloomStrength: 5,
+      bloomThreshold: 0,
+      bloomRadius: 0,
+      scene: 'Scene with Glow',
+    }
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: '#extension GL_OES_standard_derivatives : enable',
+      },
+      uniforms: {
+        uLight: { value: new THREE.Vector3() },
+        resolution: { value: new THREE.Vector4() },
+        time: { value: 0 },
+        // texture  1: { value: new THREE.TextureLoader().load('/textures/texture1.jpg') },
+        progress: { value: 0 },
+      },
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    })
+
+    this.intersects = []
+
+    /*******************
+     * 4. Init
+     * *****************/
+    this.init()
+    this.setPositionsAddScene()
+    // this.postProcessing()
+    this.attachHandlers()
+    gsap.ticker.add(this.render)
+  }
+
+  postProcessing() {
+    const renderScene = new RenderPass(this.scene, this.camera)
+
+    const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85)
+    bloomPass.threshold = this.params.bloomThreshold
+    bloomPass.strength = this.params.bloomStrength
+    bloomPass.radius = this.params.bloomRadius
+
+    const bloomComposer = new EffectComposer(this.renderer)
+    bloomComposer.renderToScreen = false
+    bloomComposer.addPass(renderScene)
+    bloomComposer.addPass(bloomPass)
+
+    const finalPass = new ShaderPass(
+      new THREE.ShaderMaterial({
+        uniforms: {
+          baseTexture: { value: null },
+          bloomTexture: { value: bloomComposer.renderTarget2.texture },
+        },
+        vertexShader: document.getElementById('vertexshader').textContent,
+        fragmentShader: document.getElementById('fragmentshader').textContent,
+        defines: {},
+      }), 'baseTexture',
+    )
+    finalPass.needsSwap = true
+
+    const finalComposer = new EffectComposer(this.renderer)
+    finalComposer.addPass(renderScene)
+    finalComposer.addPass(finalPass)
+  }
+
+  setPositionsAddScene() {
+    this.mouseLightPlane.position.set(this.mouse.x, this.mouse.y, 0)
+    this.mouseLight.add(this.mouseLightSphere)
+    this.scene.add(this.mouseLightPlane)
+    this.scene.add(this.mouseLight)
+  }
+
+  init() {
+    this.cursor.setAttribute('id', 'cursor')
+    this.cursor.style.position = 'fixed'
+    this.cursor.style.top = '0'
+    this.cursor.style.left = '0'
+    this.cursor.style.width = '100px'
+    this.cursor.style.height = '100px'
+    this.cursor.style.background = 'red'
+    this.cursor.style.borderRadius = '50%'
+    this.cursor.style.transform = 'translate(-50%, -50%)'
+    this.cursor.style.zIndex = 99999999999
+    document.body.appendChild(this.cursor)
+
+    /**********************
+     * Mouse Light Plane
+     * @type {THREE.PlaneGeometry}
+     * @type {THREE.MeshBasicMaterial}
+     * @type {THREE.Mesh}
+    */
+    const planeGeometry = new THREE.PlaneGeometry(2, 1, 2, 2)
+    const planeMaterial = this.material
+    this.mouseLightPlane = new THREE.Mesh(planeGeometry, planeMaterial)
+    this.mouseLightPlane.receiveShadow = true
+    /**********************
+     * Mouse Light Sphere
+     * @type {THREE.SphereGeometry}
+     *  @type {THREE.MeshBasicMaterial}
+      * @type {THREE.Mesh}
+      * **/
+    const sphereGeometry = new THREE.SphereGeometry(this.sphereSize, 32, 32)
+    const sphereMaterial = this.material
+    // sphereMaterial.color.multiplyScalar(this.lightIntensity)
+    this.mouseLightSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+    this.mouseLightSphere.castShadow = true
+
+    /**********************
+     * Mouse Light
+     * @type {THREE.PointLight}
+     * @type {THREE.SphereGeometry}
+     * @type {THREE.MeshBasicMaterial}
+     * @type {THREE.Mesh}
+      */
+    this.mouseLight = new THREE.PointLight(0xFFFFFF, 100, 100)
+    this.mouseLight.intensity = this.lightIntensity
+    // this.mouseLight.color.multiplyScalar(this.lightIntensity)
+    this.mouseLight.castShadow = true
+    this.mouseLight.shadow.bias = -0.005
+    this.mouseLight.shadow.mapSize.width = 1024
+    this.mouseLight.shadow.mapSize.height = 1024
+    this.mouseLight.shadow.camera.near = 0.1
+    this.mouseLight.shadow.camera.far = 500
+    this.mouseLight.shadow.camera.fov = 30
+    this.mouseLight.add(this.mouseLightSphere)
+    /**********************
+     * Raycaster
+     * @type {THREE.Raycaster}
+     * @type {THREE.Vector2}
+     **/
+    this.raycaster = new THREE.Raycaster()
+  }
+
+  attachHandlers() {
+    window.addEventListener('mousemove', (event) => {
+      this.prevPos.x = this.mouse.x
+      this.prevPos.y = this.mouse.y
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+
+      this.raycaster.setFromCamera(this.mouse, this.camera)
+
+      this.eMouse.x = event.clientX
+      this.eMouse.y = event.clientY
+
+      this.intersects = this.raycaster.intersectObjects([this.mouseLightPlane])
+      if (this.intersects.length > 0) {
+        const p = this.intersects[0].point
+        this.eMouse.x = p.x
+        this.eMouse.y = p.y
+      }
+    })
+  }
+
+  render() {
+    document.getElementById('cursor').style.transform = `translate(${this.elasticMouse.x}px, ${this.elasticMouse.y}px)`
+
+    this.temp.copy(this.eMouse).sub(this.elasticMouse).multiplyScalar(0.15)
+    this.elasticMouseVel.add(this.temp)
+    this.elasticMouseVel.multiplyScalar(0.85)
+    this.elasticMouse.add(this.elasticMouseVel)
+
+    this.mouseLight.position.copy(this.elasticMouse.position)
+
+    this.material.uniforms.uLight.value = this.mouseLight.position
+    this.material.uniforms.time.value = Date.now() * 0.0005
+
+    this.renderer.render(this.scene, this.camera)
+  }
+}
